051de5894ce90cbeb3a2a29fa51c34ab098ecfc4
sinkyoungdeok <tkagmd1@naver.com>
2024-08-08 01:56:14 +0900

    X-Elastic-Product 해결 필요

--- src/main/kotlin/com/restaurant/be/common/config/EsConfig.kt
+++ src/main/kotlin/com/restaurant/be/common/config/EsConfig.kt
@@ -2,9 +2,12 @@ package com.restaurant.be.common.config
 
 import com.jillesvangurp.ktsearch.KtorRestClient
 import com.jillesvangurp.ktsearch.SearchClient
+import io.jsonwebtoken.Jwts.header
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.java.Java
 import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.header
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -37,6 +40,10 @@ fun ktorClientWithJavaEngine(
 
     install(HttpTimeout) {
         requestTimeoutMillis = timeoutMillis
+    }
+
+    defaultRequest {
+        header("X-Elastic-Product", "Elasticsearch")
     }
 
 //    로컬 환경에서만 enable해서 사용
